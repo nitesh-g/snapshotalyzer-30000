3bb1beccbdd585df1b057396cc3657920c82462a
Nitesh Gupta <niteshgupta@Niteshs-MacBook-Pro.local>
2020-07-05 10:41:33 -0400

    added click module and changed the way to print instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,24 @@
 import boto3
+import click
+
+print('\n')
+session = boto3.Session(profile_name="shotty")
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "list EC2 instances"
+    for i in ec2.instances.all():
+        print(','.join((
+          i.id,
+          i.instance_type,
+          i.placement['AvailabilityZone'],
+          i.state['Name'],
+          i.public_dns_name
+        )))
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name="shotty")
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+  list_instances()
+
+print('\n')
+
